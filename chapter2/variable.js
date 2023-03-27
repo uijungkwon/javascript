5666a62ed6817e4ce7f19b0aee7fa0d1607b91b5
uijungkwon <kuj08036410@gmail.com>
2023-03-27 10:12:50 +0900

    delete files

--- chapter2/variable.js
+++
@@ -1,4 +0,0 @@
-let pi = 3.14159265;
-let radius = 10;
-console.log(`둘레: ${2 * pi * radius}`);
-console.log(`넓이: ${pi * radius * radius}`);
--- chatper3/time.js
+++
@@ -1,8 +0,0 @@
-/*현재 시간 구하기*/
-let date = new Date();
-console.log(date.getFullYear());
-console.log(date.getMonth() + 1);
-console.log(date.getDay());
-console.log(date.getHours());
-console.log(date.getMinutes());
-console.log(date.getSeconds());
